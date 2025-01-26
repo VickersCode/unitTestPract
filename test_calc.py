14152b78869973f48883de28472b230710e38edd
VickersCode <tyler31vickers@gmail.com>
2025-01-25 20:16:09 -0800

    testing auth

--- test_calc.py
+++ test_calc.py
@@ -4,7 +4,7 @@ import calc
 class TestCalc(unittest.TestCase):
 
     def test_add(self):
-        result = calc.add(10, 5)
+        result = calc.add(1, 5)
         self.assertEqual(result, 15)
 
 if __name__ == '__main__':
